10e2b491af6f1b59b8d5bf46e904b14f87b6101d
Lzn84 <luizmiguel.contato84@gmail.com>
2024-10-31 08:54:19 -0300

    Delete Aula Desafio2.py

--- Aula Desafio2.py
+++
@@ -1,4 +0,0 @@
-dia = input('Vamos fazer sua data de nascimento insira seu dia')
-mes = input('Qual mês')
-ano = input('Qual ano')
-print('Você nasceu no dia', dia, 'no mês', mes, 'no ano', ano, 'certo?')
